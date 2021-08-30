8a84a4c347cc2a992813cdd0c1de0e2cf79bb077
haotheheck <74411568+haotheheck@users.noreply.github.com>
2021-08-30 22:04:57 +0100

    Add files via upload

---
+++ Software Testing/CS265_Lab_w02Test.java
@@ -0,0 +1,16 @@
+package cs265;
+
+import org.testng.annotations.Test;
+
+public class CS265_Lab_w02Test {
+
+  @Test
+  public void method1Test() {
+    throw new RuntimeException("Test not implemented");
+  }
+
+  @Test
+  public void method2Test() {
+    throw new RuntimeException("Test not implemented");
+  }
+}
